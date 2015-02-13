38a4b4b4222971f10fad3bd8d880a85463b5f733
Eric Crosson <esc@ericcrosson.com>
2015-02-12 19:43:35 -0600

    added dictionary

---
+++ emacs/.emacs.d/elpa/connection-20140717.2029/connection-autoloads.el
@@ -0,0 +1,15 @@
+;;; connection-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("connection.el") (21725 22007 758496 718000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; connection-autoloads.el ends here
