682bf8e55642ae30a804865ac8cec09d3e0df5dd
Eric Crosson <esc@ericcrosson.com>
2015-02-12 23:02:39 -0600

    hello \\[mode-line-in-header]

--- emacs/.emacs.d/site-lisp/esc-lisp/hidden-mode-line-mode/hidden-mode-line-mode.el
+++ emacs/.emacs.d/site-lisp/esc-lisp/hidden-mode-line-mode/hidden-mode-line-mode.el
@@ -23,3 +23,15 @@
      0 nil 'message
      (concat "Hidden Mode Line Mode enabled.  "
              "Use M-x hidden-mode-line-mode to make the mode-line appear."))))
+
+(defvar-local header-line-format nil "Current state of header-line-format")
+
+;;;###autoload
+(defun mode-line-in-header ()
+  (interactive)
+  (if (not header-line-format)
+      (setq header-line-format mode-line-format
+            mode-line-format nil)
+    (setq mode-line-format header-line-format
+          header-line-format nil))
+  (set-window-buffer nil (current-buffer)))
