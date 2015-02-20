f7e767fb9f391eafae828a2b5f33d626164dc8cc
Eric Crosson <esc@ericcrosson.com>
2015-02-19 21:13:57 -0600

    hello writegood-mode

---
+++ emacs/.emacs.d/site-lisp/elpa/writegood-mode-20140605.734/writegood-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "writegood-mode" "20140605.734" "Polish up poor writing on the fly" 'nil :url "http://github.com/bnbeckwith/writegood-mode" :keywords '("writing" "weasel-words" "grammar"))
