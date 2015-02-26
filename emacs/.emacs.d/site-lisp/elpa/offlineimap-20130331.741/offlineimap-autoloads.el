42d996cdd045f5447c89ece8c0595be85a5c0820
Eric Crosson <esc@ericcrosson.com>
2015-02-25 19:23:33 -0600

    hello offlineimap

---
+++ emacs/.emacs.d/site-lisp/elpa/offlineimap-20130331.741/offlineimap-autoloads.el
@@ -0,0 +1,22 @@
+;;; offlineimap-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "offlineimap" "offlineimap.el" (21742 22922
+;;;;;;  326802 583000))
+;;; Generated autoloads from offlineimap.el
+
+(autoload 'offlineimap "offlineimap" "\
+Start OfflineIMAP.
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; offlineimap-autoloads.el ends here
---
+++ emacs/.emacs.d/site-lisp/elpa/offlineimap-20130331.741/offlineimap-pkg.el
@@ -0,0 +1,1 @@
+(define-package "offlineimap" "20130331.741" "Run OfflineIMAP from Emacs" 'nil :url "http://julien.danjou.info/offlineimap-el.html")
