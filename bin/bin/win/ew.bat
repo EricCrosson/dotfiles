ac358dfa0b5eb6c0597ad73574dd9116778eba7a
Eric Crosson <esc@ericcrosson.com>
2015-05-18 00:49:40 -0500

    added bin dir

---
+++ bin/bin/win/ew.bat
@@ -0,0 +1,2 @@
+cd C:\cygwin\home\ecrosson
+wscript.exe "C:\cygwin\home\ecrosson\bin\win\invisible.vbs" "C:\cygwin\home\ecrosson\bin\usr\win\emacs-24.3\bin\emacsclientw.exe -c -a C:\cygwin\home\ecrosson\bin\usr\win\emacs-24.3\bin\runemacs.exe"
