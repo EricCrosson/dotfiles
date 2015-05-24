3ed1a6681e4c9f26073d457c8e163c85526cfcaf
Eric Crosson <esc@ericcrosson.com>
2015-05-23 17:54:38 -1000

    hello dired
    many thanks to
    http://www.reddit.com/r/emacs/comments/18qa15/dired_discussion/

---
+++ emacs/.emacs.d/private/dired/packages.el
@@ -0,0 +1,32 @@
+;;; packages.el --- dired Layer packages File for Spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defvar dired-packages '(dired-details+))
+
+(defun dired/init-dired-details+ ()
+  "Initialize `dired-details+'."
+  (setq dired-details-propagate-flag nil)
+
+  (after "dired-aux"
+    (unless (boundp dired-guess-shell-alist-user)
+      (setq dired-guess-shell-alist-user '())
+    (add-to-list 'dired-guess-shell-alist-user '("\\.mp4$" "vlc" "mplayer"))
+    (add-to-list 'dired-guess-shell-alist-user '("\\.pdf$" "evince"))
+    (add-to-list 'dired-compress-file-suffixes '("\\.zip$" "unzip")))
+  (setq dired-listing-switches "-alhv")
+  (setq dired-recursive-copies 'always)
+  (after "ibuf-ext"
+    (add-to-list 'ibuffer-saved-filter-groups '("default" ("dired" (mode . dired-mode)))))
+
+  ;; Allow running multiple async commands simultaneously
+  (defadvice shell-command (after shell-in-new-buffer
+                             (command &optional output-buffer error-buffer))
+    (when (get-buffer "*Async Shell Command*")
+      (with-current-buffer "*Async Shell Command*"
+        (rename-uniquely))))
+  (ad-activate 'shell-command)
+
+  (setq dired-dwim-target t))
