514599b3cece35d16e592caba174f91d2f991b45
Eric Crosson <esc@ericcrosson.com>
2015-05-23 18:00:14 -1000

    add misc-cmds

---
+++ emacs/.emacs.d/private/misc-cmds/packages.el
@@ -0,0 +1,16 @@
+;;; packages.el --- misc-cmds Layer packages File for Spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defvar misc-cmds-packages '(misc-cmds)) 
+
+(defun misc-cmds/init-misc-cmds ()
+  "Initialize `misc-cmds'."
+  (use-package misc-cmds
+    :commands revert-buffer-no-confirm
+    :config
+    (evil-leader/set-key
+      "rn" 'revert-buffer-no-confirm))
+  )
