714d820dbd2a9030e7c648fab9dfca41d4f85cb4
Eric Crosson <esc@ericcrosson.com>
2015-06-18 22:04:24 -0500

    added bindings to ibuffer-extras' packages

--- emacs/.emacs.d/private/ibuffer-extras/packages.el
+++ emacs/.emacs.d/private/ibuffer-extras/packages.el
@@ -8,8 +8,15 @@
 
 (defun ibuffer-extras/init-ibuffer-vc ()
   "Initialize `ibuffer-vc'."
-  (use-package ibuffer-vc))
+  (use-package ibuffer-vc
+    :config
+    (after 'ibuffer
+       (define-key ibuffer-mode-map (kbd "/ v") 'ibuffer-filter-by-vc-root)
+       )))
 
 (defun ibuffer-extras/init-ibuffer-tramp ()
   "Initialize `ibuffer-tramp'."
-  (use-package ibuffer-tramp))
+  (use-package ibuffer-tramp
+    :config
+    (after 'ibuffer
+      (define-key ibuffer-mode-map (kbd "/ c") 'ibuffer-filter-by-tramp-connection))))
