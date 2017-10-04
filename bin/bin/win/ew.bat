9d37133b15b53de94226048cd455c4d1e7409a85
Eric Crosson <ecrosson@shoretel.com>
2017-10-04 16:30:13 -0500

    Remove deleted files

--- bin/bin/win/ew.bat
+++
@@ -1,2 +0,0 @@
-cd C:\cygwin\home\ecrosson
-wscript.exe "C:\cygwin\home\ecrosson\bin\win\invisible.vbs" "C:\cygwin\home\ecrosson\bin\usr\win\emacs-24.3\bin\emacsclientw.exe -c -a C:\cygwin\home\ecrosson\bin\usr\win\emacs-24.3\bin\runemacs.exe"
