03f18bd3661545c57f64b131a3e86b1bd77e3409
Eric Crosson <eric.s.crosson@utexas.edu>
2022-01-17 11:25:47 -0600

    feat: track helix editor via git submodule

---
+++ run_after_21-record-git-submodule-shas.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+readonly chezmoi_dir="${HOME}/.local/share/chezmoi"
+readonly git_submodule_sha_dir="${chezmoi_dir}/.git-submodule-shas"
+
+cd "${chezmoi_dir}" || exit
+mkdir -p "${git_submodule_sha_dir}"
+
+for submodule in "$(git submodule status)"
+do
+  repo="$(echo "$submodule" | cut -d' ' -f3)"
+  hash="$(echo "$submodule" | cut -d' ' -f2)"
+
+  echo "$hash" > "${git_submodule_sha_dir}/${repo}"
+done
