30d51387f7830579075c629420e4d54f85f10fa7
Eric Crosson <eric.s.crosson@utexas.edu>
2022-04-25 10:43:16 -0500

    fix(1password): improve idempotency

--- run_once_after_21-install-1password.sh
+++ run_once_after_21-install-1password.sh
@@ -5,8 +5,10 @@
 # Instructions from https://support.1password.com/install-linux/
 
 # 1. Add the key for the 1Password apt repository
-curl -sS https://downloads.1password.com/linux/keys/1password.asc \
-  | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+if [ ! -f /usr/share/keyrings/1password-archive-keyring.gpg ]; then
+  curl -sS https://downloads.1password.com/linux/keys/1password.asc \
+    | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+fi
 
 # 2. Add the 1Password apt repository
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' \
@@ -14,9 +16,15 @@ echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gp
 
 # 3. Add the debsig-verify policy
 sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol \
+  | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+
 sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+if [ ! -f /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg ]; then
+  curl -sS https://downloads.1password.com/linux/keys/1password.asc \
+    | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+fi
 
 # 4. Install 1Password
-sudo apt update && sudo apt install 1password
+sudo apt update
+sudo apt install -y 1password
