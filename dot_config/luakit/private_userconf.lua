f701eae1d37dc33c789a9fe1cf64dffb4b751de8
Eric Crosson <eric.s.crosson@utexas.edu>
2022-05-21 08:23:04 -0500

    fix(luakit): bind import to variable
    feat(luakit): set zoom on tiny website

--- dot_config/luakit/private_userconf.lua
+++ dot_config/luakit/private_userconf.lua
@@ -1,5 +1,7 @@
 local settings = require "settings"
 
+-- search engines
+
 local engines = settings.window.search_engines
 engines.aur    = "https://aur.archlinux.org/packages.php?O=0&K=%s&do_Search=Go"
 engines.aw     = "https://wiki.archlinux.org/index.php/Special:Search?fulltext=Search&search=%s"
@@ -7,5 +9,9 @@ engines.google = "https://www.google.com/search?name=f&hl=en&q=%s"
 
 engines.default = engines.google
 
-require "downloads"
+-- zoom levels
+
+settings.on["news.ycombinator.com"].webview.zoom_level = 125
+
+local downloads = require "downloads"
 downloads.default_dir = os.getenv("HOME") .. "/Downloads"
