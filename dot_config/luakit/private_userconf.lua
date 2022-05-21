5ba6fbea5303661d6b09cd983e4f953e355d769b
Eric Crosson <eric.s.crosson@utexas.edu>
2022-05-21 15:04:08 -0500

    chore(luakit): remove unused settings

--- dot_config/luakit/private_userconf.lua
+++ dot_config/luakit/private_userconf.lua
@@ -3,7 +3,6 @@ local settings = require "settings"
 -- search engines
 
 local engines = settings.window.search_engines
-engines.aur    = "https://aur.archlinux.org/packages.php?O=0&K=%s&do_Search=Go"
 engines.aw     = "https://wiki.archlinux.org/index.php/Special:Search?fulltext=Search&search=%s"
 engines.google = "https://www.google.com/search?name=f&hl=en&q=%s"
 
@@ -12,6 +11,7 @@ engines.default = engines.google
 -- zoom levels
 
 settings.on["news.ycombinator.com"].webview.zoom_level = 125
+-- settings.webview.minimum_font_size = 10
 
 local downloads = require "downloads"
 downloads.default_dir = os.getenv("HOME") .. "/Downloads"
