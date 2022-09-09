809f35f5affad44de2587161aaee5736f2d78d90
Eric Crosson <eric.s.crosson@utexas.edu>
2022-09-09 10:58:02 -0500

    feat: install jsonnet language server
    :tada:

---
+++ run_once_after_62-install-jsonnet-language-server.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# From https://github.com/grafana/jsonnet-language-server/releases
+# v0.9.0
+download_link="https://github.com/grafana/jsonnet-language-server/releases/download/v0.9.0/jsonnet-language-server_0.9.0_linux_amd64"
+
+destination="$HOME/.local/bin/jsonnet-language-server"
+
+if [[ ! -f "${destination}" ]]
+then
+
+  curl --output "${destination}" "${download_link}"
+  chmod +x "${destination}"
+
+fi
