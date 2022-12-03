b831453be4dae2124db9be65e15aa31742ee2f97
Eric Crosson <eric.s.crosson@utexas.edu>
2022-12-03 16:59:58 -0600

    refactor: split home-manager config into imports

---
+++ home/editor/helix/default.nix
@@ -0,0 +1,51 @@
+{
+  pkgs,
+  system,
+  user,
+  inputs,
+  ...
+}: {
+  home.packages = with pkgs; [
+    inputs.jsonnet-language-server.packages.${system}.jsonnet-tool
+    hadolint
+    ltex-ls
+    nil
+    nodePackages.bash-language-server
+    nodePackages.dockerfile-language-server-nodejs
+    nodePackages.typescript-language-server
+    nodePackages.vscode-langservers-extracted
+    rust-analyzer
+    shellcheck
+    taplo-lsp # TOML
+  ];
+
+  programs.helix = {
+    enable = true;
+    package = inputs.helix.packages.${system}.default;
+    languages = [
+      {
+        name = "markdown";
+        language-server.command = "ltex-ls";
+      }
+    ];
+    settings = {
+      theme = "catppuccin_${user.theme}";
+      keys.normal = {
+        C-h = "jump_view_left";
+        C-j = "jump_view_down";
+        C-k = "jump_view_up";
+        C-l = "jump_view_right";
+        space.t = ":tree-sitter-subtree";
+      };
+      editor = {
+        idle-timeout = 0;
+        cursor-shape = {
+          normal = "block";
+          insert = "bar";
+          select = "underline";
+        };
+        whitespace.render.tab = "all";
+      };
+    };
+  };
+}
