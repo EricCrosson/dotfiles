74edf04990bcb57dc9d57bf2cf52995606c43187
Eric Crosson <eric.s.crosson@utexas.edu>
2022-12-24 11:21:47 -0600

    chore: remove unused scripts
    so long chezmoi

--- run_once_after_31-install-rust.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-install_rust() {
-  if ! command -v rustc &>/dev/null; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
-  fi
-}
-
-install_rust
--- run_once_after_37-install-haskell-stack.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-install_stack() {
-  if ! command -v stack &>/dev/null; then
-    curl -sSL https://get.haskellstack.org/ | sh
-  fi
-}
-
-install_stack
