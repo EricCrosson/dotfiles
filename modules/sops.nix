c5f3167f2d89f89a20440078164790a26d1adf3c
Eric Crosson <eric.s.crosson@utexas.edu>
2023-01-02 07:33:03 -0600

    chore: add wakatime secret

--- modules/sops.nix
+++ modules/sops.nix
@@ -1,7 +1,12 @@
-{ config, ...}:
-
-{
+{config, ...}: {
   sops.defaultSopsFile = ../secrets/main.yaml;
+  sops.secrets.wakatime = {
+    mode = "0400";
+    # TODO: use variable for username
+    owner = config.users.users.eric.name;
+    # TODO: use more restrictive group
+    group = "users";
+  };
   sops.secrets.github_token_personal = {
     mode = "0400";
     # TODO: use variable for username
