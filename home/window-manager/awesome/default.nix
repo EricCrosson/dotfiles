9804b6c7e8f3d90bb829cb340a52990e3f872df6
Eric Crosson <eric.s.crosson@utexas.edu>
2023-01-14 18:46:21 -0600

    feat: add catppuccin theme for rofi

--- home/window-manager/awesome/default.nix
+++ home/window-manager/awesome/default.nix
@@ -1,14 +1,37 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  user,
+  ...
+}: {
   home = {
     packages = with pkgs; [
       pamixer
       pavucontrol # Graphival audio control
       playerctl
-      rofi
     ];
 
     file = {
       ".config/awesome/".source = ../../../.config/awesome;
+      ".local/share/rofi/themes/catppuccin-frappe.rasi".source = ../../../.local/share/rofi/themes/catppuccin-frappe.rasi;
+      ".local/share/rofi/themes/catppuccin-latte.rasi".source = ../../../.local/share/rofi/themes/catppuccin-latte.rasi;
+      ".local/share/rofi/themes/catppuccin-macchiato.rasi".source = ../../../.local/share/rofi/themes/catppuccin-macchiato.rasi;
+      ".local/share/rofi/themes/catppuccin-mocha.rasi".source = ../../../.local/share/rofi/themes/catppuccin-mocha.rasi;
+    };
+  };
+
+  programs.rofi = {
+    enable = true;
+    plugins = [pkgs.rofi-calc];
+    terminal = "${pkgs.kitty}/bin/kitty";
+    theme = "catppuccin-${user.preferences.theme}";
+    extraConfig = {
+      modi = "run,drun,window";
+      icon-theme = "Oranchelo";
+      show-icons = true;
+      drun-display-format = "{icon} {name}";
+      disable-history = false;
+      hide-scrollbar = true;
+      sidebar-mode = true;
     };
   };
 }
