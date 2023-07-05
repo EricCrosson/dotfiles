4b8208b2b1929c482d046e58c004f8b6045e1c23
Eric Crosson <eric.s.crosson@utexas.edu>
2023-07-05 09:24:31 -0500

    chore(deps): use git-disjoint beta

--- flake.nix
+++ flake.nix
@@ -82,7 +82,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     git-disjoint = {
-      url = "github:ericcrosson/git-disjoint";
+      url = "github:ericcrosson/git-disjoint/beta";
       inputs.crane.follows = "crane";
       inputs.nixpkgs.follows = "nixpkgs";
     };
