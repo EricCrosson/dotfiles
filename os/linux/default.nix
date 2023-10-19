ce416845ca9bccdacfc54688971b2f1437baac46
Eric Crosson <eric.s.crosson@utexas.edu>
2023-10-19 09:19:41 -0500

    chore: remove simplenote
    The package is no longer maintained, got to use the web app

--- os/linux/default.nix
+++ os/linux/default.nix
@@ -1,14 +1,10 @@
 {pkgs, ...}: {
   home = {
-    packages = with pkgs;
-      [
-        dtrx
-        evtest
-        maim
-      ]
-      ++
-      # These packages are only supported on x86_64-linux
-      pkgs.lib.optionals (pkgs.system == "x86_64-linux") [simplenote];
+    packages = with pkgs; [
+      dtrx
+      evtest
+      maim
+    ];
 
     file = {
       # DISCUSS: can we use a nix-provided path to this file?
