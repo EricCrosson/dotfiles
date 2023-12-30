e5d6dde0df34c135a54ac4a1d1f008945c11b9fc
Eric Crosson <eric.s.crosson@utexas.edu>
2023-12-30 16:27:51 -0600

    fix: avoid using google-chrome on unsupported system
    The unsupported system is aarch64-linux

--- os/linux/default.nix
+++ os/linux/default.nix
@@ -1,13 +1,23 @@
 {pkgs, ...}: {
-  home = {
-    packages = with pkgs; [
-      alsa-utils
-      dtrx
-      evtest
-      google-chrome
-      maim
-      vlc
-    ];
+  home = let
+    system_specific_packages = with pkgs;
+      {
+        "x86_64-linux" = [
+          google-chrome
+        ];
+        "aarch64-linux" = [];
+      }
+      ."${pkgs.system}";
+  in {
+    packages = with pkgs;
+      [
+        alsa-utils
+        dtrx
+        evtest
+        maim
+        vlc
+      ]
+      ++ system_specific_packages;
 
     file = {
       # DISCUSS: can we use a nix-provided path to this file?
