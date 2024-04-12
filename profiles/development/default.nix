b7bda77c7f67eead6a28441d9ec3daf067a4f875
Eric Crosson <eric.s.crosson@utexas.edu>
2024-04-12 13:31:09 -0500

    fix: add libiconv to darwin development environment

--- profiles/development/default.nix
+++ profiles/development/default.nix
@@ -9,42 +9,48 @@
     homeDirectory = "${user.homeDirectory}";
     stateVersion = "22.05";
 
-    packages = with pkgs; [
-      inputs.bash-barrier.packages.${pkgs.system}.default
-      inputs.batch-edit-prs.packages.${pkgs.system}.default
-      inputs.git-diff-regex.packages.${pkgs.system}.default
-      inputs.git-disjoint.packages.${pkgs.system}.default
-      inputs.git-dl.packages.${pkgs.system}.default
-      inputs.git-review.packages.${pkgs.system}.default
-      inputs.npm-dep-version.packages.${pkgs.system}.default
-      inputs.nurl.packages.${pkgs.system}.default
+    packages = with pkgs;
+      [
+        inputs.bash-barrier.packages.${pkgs.system}.default
+        inputs.batch-edit-prs.packages.${pkgs.system}.default
+        inputs.git-diff-regex.packages.${pkgs.system}.default
+        inputs.git-disjoint.packages.${pkgs.system}.default
+        inputs.git-dl.packages.${pkgs.system}.default
+        inputs.git-review.packages.${pkgs.system}.default
+        inputs.npm-dep-version.packages.${pkgs.system}.default
+        inputs.nurl.packages.${pkgs.system}.default
 
-      # This isn't specific to development but it takes a system that can
-      # compile Rust from source
-      inputs.percentage-changed-calculator.packages.${pkgs.system}.default
+        # This isn't specific to development but it takes a system that can
+        # compile Rust from source
+        inputs.percentage-changed-calculator.packages.${pkgs.system}.default
 
-      # DISCUSS: switch between nightly and precompiled releases
-      (fenix.complete.withComponents [
-        "cargo"
-        "clippy"
-        "rustc"
-        "rustfmt"
-        "rust-src"
-      ])
-      rust-analyzer-nightly
-      cargo-tarpaulin
-      cargo-watch
+        # DISCUSS: switch between nightly and precompiled releases
+        (fenix.complete.withComponents [
+          "cargo"
+          "clippy"
+          "rustc"
+          "rustfmt"
+          "rust-src"
+        ])
+        rust-analyzer-nightly
+        cargo-tarpaulin
+        cargo-watch
 
-      # Allows cargo to build Rust projects that don't have a dev shell
-      gcc
+        # Allows cargo to build Rust projects that don't have a dev shell
+        gcc
 
-      bc
-      csvq
-      duckdb
-      vscode
+        bc
+        csvq
+        duckdb
+        vscode
 
-      # TypeScript
-      yarn
-    ];
+        # TypeScript
+        yarn
+      ]
+      ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+        pkgs.darwin.apple_sdk.frameworks.Security
+        pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+        pkgs.libiconv
+      ];
   };
 }
