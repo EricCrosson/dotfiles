a749b67fdef2862d0632051a98c8296dc678551e
Eric Crosson <eric.s.crosson@utexas.edu>
2024-05-01 17:52:13 -0500

    feat(darwin): use pinentry-curses

--- hosts/MBP-0954/configuration.nix
+++ hosts/MBP-0954/configuration.nix
@@ -1,7 +1,4 @@
-{pkgs, ...}: let
-  # TODO: use ncurses-pinentry
-  pinentry-program = "${pkgs.pinentry_mac}/Applications/pinentry-mac.app/Contents/MacOS/pinentry-mac";
-in {
+{pkgs, ...}: {
   config = {
     environment = {
       # List packages installed in system profile. To search by name, run:
@@ -16,38 +13,17 @@ in {
       };
     };
 
-    # Create /etc/zshrc that loads the nix-darwin environment.
-    programs.zsh.enable = true;
-
-    # Required to use flakes, which are an experimental module
-    nix.package = pkgs.nixUnstable;
-
-    nix.extraOptions = ''
-      keep-derivations = true
-      keep-outputs = true
-      experimental-features = nix-command flakes
-
-      min-free = ${toString (20 * 1024 * 1024 * 1024)}
-      max-free = ${toString (30 * 1024 * 1024 * 1024)}
-    '';
-
-    nix = {
-      gc = {
-        user = "ericcrosson";
-        automatic = true;
-        interval = {
-          Weekday = 0;
-          Hour = 0;
-          Minute = 0;
+    programs = {
+      gnupg = {
+        agent = {
+          enable = true;
+          enableSSHSupport = true;
         };
-        options = "--delete-older-than 7d";
       };
-    };
 
-    programs.gnupg = {
-      agent = {
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      zsh = {
         enable = true;
-        enableSSHSupport = true;
       };
     };
 
@@ -57,7 +33,7 @@ in {
           gpg-agent = {
             target = ".gnupg/gpg-agent.conf";
             text = ''
-              pinentry-program ${pinentry-program}
+              pinentry-program ${pkgs.pinentry-curses}/bin/pinentry-curses
               default-cache-ttl 43200
               default-cache-ttl-ssh 43200
               max-cache-ttl 43200
@@ -75,8 +51,35 @@ in {
       }
     ];
 
-    home-manager.useUserPackages = true;
-    home-manager.useGlobalPkgs = true;
+    nix = {
+      # Required to use flakes, which are an experimental module
+      package = pkgs.nixUnstable;
+
+      extraOptions = ''
+        keep-derivations = true
+        keep-outputs = true
+        experimental-features = nix-command flakes
+
+        min-free = ${toString (20 * 1024 * 1024 * 1024)}
+        max-free = ${toString (30 * 1024 * 1024 * 1024)}
+      '';
+
+      gc = {
+        user = "ericcrosson";
+        automatic = true;
+        interval = {
+          Weekday = 0;
+          Hour = 0;
+          Minute = 0;
+        };
+        options = "--delete-older-than 7d";
+      };
+    };
+
+    home-manager = {
+      useUserPackages = true;
+      useGlobalPkgs = true;
+    };
 
     # Auto upgrade nix package and the daemon service.
     services.nix-daemon.enable = true;
