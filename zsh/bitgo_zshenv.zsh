9fb4c44ca32db4e40d5b3253539dd8e43c63dfb7
Eric Crosson <eric.s.crosson@utexas.edu>
2024-07-17 11:34:28 -0500

    chore(MBP-0954): source Jira API token

--- zsh/bitgo_zshenv.zsh
+++ zsh/bitgo_zshenv.zsh
@@ -1,3 +1,5 @@
 GITHUB_TOKEN="$(cat "$HOME/.config/sops-nix/secrets/github_token_bitgo" 2>/dev/null || echo "")"
 export GITHUB_TOKEN
+JIRA_API_TOKEN="$(cat "$HOME/.config/sops-nix/secrets/jira_token_bitgo" 2>/dev/null || echo "")"
+export JIRA_API_TOKEN
 export NIX_CONFIG="access-tokens = github.com=${GITHUB_TOKEN}"
