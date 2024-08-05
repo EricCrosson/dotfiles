3a2d5aa1a9a8883819285ee09124150fe81eadb4
Eric Crosson <eric.s.crosson@utexas.edu>
2024-08-05 14:56:04 -0500

    chore: add motd

--- zsh/bitgo_zshrc.zsh
+++ zsh/bitgo_zshrc.zsh
@@ -1,2 +1,8 @@
 eval "$(keychain --eval --agents ssh --quiet id_rsa)"
 eval "$(keychain --eval --agents ssh --quiet id_rsa_personal)"
+
+cat <<EOF
+The beginning of love is to let those we love be perfectly themselves,
+and not to twist them to fit our own image.
+Otherwise we love only the reflection of ourselves we find in them.
+EOF
