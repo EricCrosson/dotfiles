85aa66dd6db8fbf415db60597797128a65b6807f
Eric Crosson <eric.s.crosson@utexas.edu>
2024-12-26 14:06:04 -0600

    chore: do not use smart cd in aider

--- zsh/bitgo_zshrc.zsh
+++ zsh/bitgo_zshrc.zsh
@@ -3,6 +3,8 @@ eval "$(keychain --eval --agents ssh --quiet id_rsa_personal)"
 
 aider() {
   AWS_PROFILE=dev \
+  SMART_CD_GIT_STATUS=false \
+  SMART_CD_LS=false \
     uvx \
       --python 3.9 \
       --from git+ssh://git@github.com/BitGo/aider \
