5f5600167043afe0f9b58ef717c3593d6778f231
Eric Crosson <eric.s.crosson@utexas.edu>
2024-12-26 10:17:15 -0600

    chore: remove fortune
    It gets slurped into the aider context history

--- zsh/bitgo_zshrc.zsh
+++ zsh/bitgo_zshrc.zsh
@@ -8,9 +8,3 @@ aider() {
       --from git+ssh://git@github.com/BitGo/aider \
       aider "$@"
 }
-
-cat <<EOF
-The beginning of love is to let those we love be perfectly themselves,
-and not to twist them to fit our own image.
-Otherwise we love only the reflection of ourselves we find in them.
-EOF
