6d7bf346f2b1cd6516fb02167ee323dc632b4c3e
Eric Crosson <eric.s.crosson@utexas.edu>
2025-06-04 21:31:30 -0500

    chore: refactor
    separate code and data a bit more

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,17 +1,18 @@
 # This file provides a centralized way to access all overlays
-# defined in this directory, either individually or as a combined overlay.
 {
   # Individual overlays for selective importing
   pypandoc-disable-tests = import ./pypandoc-disable-tests.nix;
   wakatime-disable-tests = import ./wakatime-disable-tests.nix;
 
   # Combined overlay that applies all overlays in this directory
-  combined = final: prev:
-    builtins.foldl'
-    (acc: overlay: acc // (overlay final prev))
-    {}
-    [
+  combined = final: prev: let
+    overlays = [
       (import ./pypandoc-disable-tests.nix)
       (import ./wakatime-disable-tests.nix)
     ];
+  in
+    builtins.foldl'
+    (acc: overlay: acc // (overlay final prev))
+    {}
+    overlays;
 }
